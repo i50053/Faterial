338f7e85a87690721c67bbb913b8bdb9380423b2
i50053 <i50053@tuta.io>
2024-10-14 10:08:14 +0300

    feat: add solution with library & demo

---
+++ src/Faterial.Demo/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
---
+++ src/Faterial/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://schemas.faterial.com/winfx", "mf")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
