60d0f1ee32911c1a315003043a50f70887a702b4
i50053 <i50053@tuta.io>
2024-10-21 00:02:26 +0300

    feat: add corner radius shape assist

--- src/Faterial/AssemblyInfo.cs
+++ src/Faterial/AssemblyInfo.cs
@@ -2,6 +2,8 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
+[assembly: XmlnsDefinition("http://schemas.faterial.com/winfx", "Faterial.Controls")]
+
 [assembly: XmlnsPrefix("http://schemas.faterial.com/winfx", "mf")]
 
 [assembly: ComVisible(false)]
---
+++ src/Faterial/Controls/ShapeAssist.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+
+namespace Faterial.Controls
+{
+    /// <summary>
+    /// Provides attached properties for customizing the shape of an element.
+    /// </summary>
+    public static class ShapeAssist
+    {
+        /// <summary>
+        /// Identifies the <see cref="ShapeAssist" />.CornerRadius attached property.
+        /// </summary>
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius",
+            typeof(CornerRadius),
+            typeof(ShapeAssist),
+            new PropertyMetadata(new CornerRadius(0))
+        );
+
+
+        /// <summary>
+        /// Gets the value of the <see cref="ShapeAssist" />.CornerRadius attached property
+        /// from a given <see cref="DependencyObject" />.
+        /// </summary>
+        /// <param name="obj">
+        /// The element from which to read the property value.
+        /// </param>
+        /// <returns>
+        /// The value of the <see cref="ShapeAssist" />.CornerRadius attached property.
+        /// </returns>
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="ShapeAssist" />.CornerRadius attached property
+        /// to a given <see cref="DependencyObject" />.
+        /// </summary>
+        /// <param name="obj">
+        /// The element on which to set the <see cref="ShapeAssist" />.CornerRadius attached property.
+        /// </param>
+        /// <param name="value">
+        /// The property value to set.
+        /// </param>
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
