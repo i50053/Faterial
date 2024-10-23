0bcf85473ef5fa46d201f2a4683e4a803b27c69c
i50053 <i50053@tuta.io>
2024-10-23 15:39:21 +0300

    feat: add utility styles to elevation system

---
+++ src/Faterial/Controls/ElevationAssist.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+
+namespace Faterial.Controls
+{
+    /// <summary>
+    /// Provides attached properties for adjusting the elevation depth along the z-axis of an element.
+    /// </summary>
+    public static class ElevationAssist
+    {
+        /// <summary>
+        /// Identifies the <see cref="ElevationAssist" />.Level attached property.
+        /// </summary>
+        public static readonly DependencyProperty LevelProperty = DependencyProperty.RegisterAttached(
+            "Level",
+            typeof(ElevationLevel),
+            typeof(ElevationAssist),
+            new PropertyMetadata(ElevationLevel.Level0)
+        );
+
+
+        /// <summary>
+        /// Gets the value of the <see cref="ElevationAssist" />.Level attached property
+        /// from a given <see cref="DependencyObject" />.
+        /// </summary>
+        /// <param name="obj">
+        /// The element from which to read the property value.
+        /// </param>
+        /// <returns>
+        /// The value of the <see cref="ElevationAssist" />.Level attached property.
+        /// </returns>
+        public static ElevationLevel GetLevel(DependencyObject obj)
+        {
+            return (ElevationLevel)obj.GetValue(LevelProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the <see cref="ElevationAssist" />.Level attached property
+        /// to a given <see cref="DependencyObject" />.
+        /// </summary>
+        /// <param name="obj">
+        /// The element on which to set the <see cref="ElevationAssist" />.Level attached property.
+        /// </param>
+        /// <param name="value">
+        /// The property value to set.
+        /// </param>
+        public static void SetLevel(DependencyObject obj, ElevationLevel value)
+        {
+            obj.SetValue(LevelProperty, value);
+        }
+    }
+}
---
+++ src/Faterial/Controls/ElevationLevel.cs
@@ -0,0 +1,37 @@
+﻿namespace Faterial.Controls
+{
+    /// <summary>
+    /// Declares the levels of elevation depth along the z-axis.
+    /// </summary>
+    public enum ElevationLevel
+    {
+        /// <summary>
+        /// Use the 0DP elevation depth.
+        /// </summary>
+        Level0,
+        /// <summary>
+        /// Use the 1DP elevation depth.
+        /// </summary>
+        Level1,
+        /// <summary>
+        /// Use the 2DP elevation depth.
+        /// </summary>
+        Level2,
+        /// <summary>
+        /// Use the 4DP elevation depth.
+        /// </summary>
+        Level3,
+        /// <summary>
+        /// Use the 6DP elevation depth.
+        /// </summary>
+        Level4,
+        /// <summary>
+        /// Use the 8DP elevation depth.
+        /// </summary>
+        Level5,
+        /// <summary>
+        /// Use the 12DP elevation depth.
+        /// </summary>
+        Level6,
+    }
+}
